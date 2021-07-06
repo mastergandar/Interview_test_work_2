868efc7149ccabf2b5fa0c658abb5119cb615285
mastergandar <mastergandar@gmail.com>
2021-07-06 12:09:11 +0200

    Bug-fix

--- promo/management/commands/check_promo.py
+++ promo/management/commands/check_promo.py
@@ -12,18 +12,22 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         promo = kwargs['promo']
         i = 0
+        found = False
         try:
             with open('promo/result/result.json', 'r') as r:
                 promo_check = json.load(r)
-                while i in range(len(promo_check["promo"])):
+                while i in range(len(promo_check["promo"])) and found is not True:
                     group = promo_check["promo"][i]
                     for key in group:
                         if promo not in group[key]:
-                            pass
+                            found = False
                         else:
                             print('Promo-code found! Group=%s' % key)
+                            found = True
                             break
                     i += 1
+                if found is False:
+                    print('Promo-code not found!')
         except OSError as e:
             print(e)
 
